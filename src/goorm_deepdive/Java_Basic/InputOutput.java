22d58a712f33212307216c6de25624bba0205ae6
bepo03 <wjsdbsgus45@gmail.com>
2026-01-20 19:18:39 +0900

    Add: goorm_deepdive, Java_Basic 폴더 추가,  InputOupt.java, output.png 파일 추가 (딥다이브 미션)

---
+++ src/goorm_deepdive/Java_Basic/InputOutput.java
@@ -0,0 +1,19 @@
+package goorm_deepdive.Java_Basic;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("이름을 입력해주세요: ");
+        String name = scanner.next();
+
+        System.out.print("나이를 입력해주세요: ");
+        int age = scanner.nextInt();
+
+        System.out.printf("이름: %s, 나이: %d\n", name, age);
+
+    }
+}
